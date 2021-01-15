7d753e502834bc1a3be07c511e3255dcdeb2ec13
lucka-me <samzhao0727@gmail.com>
2021-01-15 23:35:41 +0800

    Register UserDefaults when launch

--- iOS/AppDelegate.swift
+++ iOS/AppDelegate.swift
@@ -21,4 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ObservableObject {
         }
         return false
     }
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        Preferences.register()
+    }
 }
