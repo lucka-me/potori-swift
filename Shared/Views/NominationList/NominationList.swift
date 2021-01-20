075a781c778d2e02cc85c5abec2d433cc23718da
lucka-me <samzhao0727@gmail.com>
2021-01-20 21:21:27 +0800

    Update nomination list for being a subview

--- Shared/Views/NominationList/NominationList.swift
+++ Shared/Views/NominationList/NominationList.swift
@@ -9,91 +9,83 @@ import SwiftUI
 
 struct NominationList: View {
     
-    #if os(iOS)
-    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
-    @EnvironmentObject var appDelegate: AppDelegate
-    #endif
-    
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.openURL) private var openURL
     @EnvironmentObject private var service: Service
     @EnvironmentObject private var filter: FilterManager
     
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    #endif
+    
+    private let fetchRequest: FetchRequest<Nomination>
+    private var nominations: FetchedResults<Nomination> {
+        fetchRequest.wrappedValue
+    }
+    
+    init(_ predicate: NSPredicate? = nil) {
+        fetchRequest = .init(
+            entity: Nomination.entity(),
+            sortDescriptors: [ NSSortDescriptor(keyPath: \Nomination.title, ascending: true) ],
+            predicate: predicate
+        )
+    }
+    
     var body: some View {
+        #if os(macOS)
+        NavigationView {
+            content.navigationTitle("view.nominations")
+        }
+        #else
         content
             .navigationTitle("view.nominations")
-            .toolbar {
-                ToolbarItem(placement: refreshPlacement) {
-                    Button(action: {
-                        service.refresh()
-                    }) {
-                        Label("view.nominations.refresh", systemImage: "arrow.clockwise")
-                    }
-                    .disabled(service.status != .idle)
-                }
-                ToolbarItem(placement: .principal) {
-                    switch service.status {
-                    case .processingMails:
-                        ProgressView(value: service.progress, total: 1.0)
-                            .frame(idealWidth: progressIdealWidth)
-                    default:
-                        Text(LocalizedStringKey(service.status.rawValue))
-                            .foregroundColor(.secondary)
-                            .font(.subheadline)
-                    }
-                }
-            }
-    }
-    
-    private var refreshPlacement: ToolbarItemPlacement {
-        #if os(macOS)
-        return .navigation
-        #else
-        if horizontalSizeClass == .compact {
-            return .navigation
-        }
-        return .primaryAction
         #endif
-    }
-    
-    private var progressIdealWidth: CGFloat {
-        #if os(macOS)
-        return 150
-        #else
-        return 100
-        #endif
+        
     }
     
     @ViewBuilder
     private var content: some View {
-        if service.status == .idle && service.isNominationsEmpty {
-            emptyPrompt
-        } else {
-            #if os(macOS)
-            List { ListContent(filter.predicate) }
-                .listStyle(PlainListStyle())
-                .frame(minWidth: 250)
-            #else
-            List { ListContent(filter.predicate) }
-                .listStyle(InsetGroupedListStyle())
-            #endif
-        }
+        #if os(macOS)
+        list
+            .listStyle(PlainListStyle())
+            .frame(minWidth: 250)
+        #else
+        list.listStyle(InsetGroupedListStyle())
+        #endif
     }
     
-    private var emptyPrompt: some View {
-        VStack {
-            if service.auth.login {
-                Text("view.nominations.refreshPrompt")
-            } else {
-                Text("view.nominations.linkPrompt")
-                Button("view.preferences.google.link") {
-                    #if os(macOS)
-                    service.auth.logIn()
-                    #else
-                    service.auth.logIn(appDelegate: appDelegate)
-                    #endif
+    @ViewBuilder
+    private var list: some View {
+        List {
+            ForEach(nominations) { nomination in
+                NavigationLink(destination: NominationDetails(nomination: nomination)) {
+                    NominationItem(nomination: nomination)
+                }
+                .contextMenu {
+                    Button(action: {
+                        openURL.callAsFunction(nomination.brainstormingURL)
+                    }) {
+                        Label("view.nominations.menuBrainstorming", systemImage: "bolt")
+                    }
+                    if nomination.hasLngLat {
+                        Button(action: {
+                            openURL.callAsFunction(nomination.intelURL)
+                        }) {
+                            Label("view.nominations.menuIntel", systemImage: "map")
+                        }
+                    }
                 }
             }
+            .onDelete { indexSet in
+                for index in indexSet {
+                    if index < nominations.endIndex {
+                        viewContext.delete(nominations[index])
+                    }
+                }
+                service.save()
+            }
+            .deleteDisabled(service.status != .idle)
         }
-        .padding()
     }
 }
 
@@ -111,71 +103,3 @@ struct NominationList_Previews: PreviewProvider {
     }
 }
 #endif
-
-fileprivate struct ListContent: View {
-    
-    @Environment(\.managedObjectContext) private var viewContext
-    @Environment(\.openURL) private var openURL
-    @EnvironmentObject private var service: Service
-    @EnvironmentObject private var filter: FilterManager
-    
-    private let fetchRequest: FetchRequest<Nomination>
-    private var nominations: FetchedResults<Nomination> {
-        fetchRequest.wrappedValue
-    }
-    
-    @State private var firstAppear = true
-    @State private var selected: String? = nil
-    
-    init(_ predicate: NSPredicate) {
-        fetchRequest = .init(
-            entity: Nomination.entity(),
-            sortDescriptors: [ NSSortDescriptor(keyPath: \Nomination.title, ascending: true) ],
-            predicate: predicate
-        )
-    }
-    
-    var body: some View {
-        let filtered = filter.filterByReason(Array(nominations))
-        ForEach(filtered) { nomination in
-            NavigationLink(
-                destination: NominationDetails(nomination: nomination),
-                tag: nomination.id,
-                selection: $selected
-            ) {
-                NominationItem(nomination: nomination)
-            }
-            .contextMenu {
-                Button(action: {
-                    openURL.callAsFunction(nomination.brainstormingURL)
-                }) {
-                    Label("view.nominations.menuBrainstorming", systemImage: "bolt")
-                }
-                if nomination.hasLngLat {
-                    Button(action: {
-                        openURL.callAsFunction(nomination.intelURL)
-                    }) {
-                        Label("view.nominations.menuIntel", systemImage: "map")
-                    }
-                }
-            }
-        }
-        .onDelete { indexSet in
-            for index in indexSet {
-                if index < filtered.endIndex {
-                    viewContext.delete(filtered[index])
-                }
-            }
-            service.save()
-        }
-        .deleteDisabled(service.status != .idle)
-        .onAppear {
-            #if os(macOS)
-            if firstAppear && selected == nil {
-                firstAppear = false
-                selected = nominations.first?.id
-            }
-            #endif
-        }
-    }
-}
