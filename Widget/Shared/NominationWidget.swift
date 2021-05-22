7a99c4f5259db60171cdc655f2a583d3c444947c
lucka-me <i@lucka.moe>
2021-05-22 12:52:40 +0800

    Update NominationWidget, for #23 - Get image data in getEntry - Refresh & notify in getTimeline

--- Widget/Shared/NominationWidget.swift
+++ Widget/Shared/NominationWidget.swift
@@ -12,57 +12,82 @@ import Intents
 struct NominationWidget: Widget {
     
     typealias ConfigurationIntent = NominationWidgetConfigurationIntent
+    typealias GetTimelineCompletionHandler = (Timeline<Entry>) -> Void
     
     struct Provider: IntentTimelineProvider {
         func placeholder(in context: Context) -> Entry {
-            .init(Date(), .init())
+            .init(.init())
         }
 
         func getSnapshot(
             for configuration: ConfigurationIntent,
             in context: Context,
-            completion: @escaping (Entry) -> ()
+            completion: @escaping (Entry) -> Void
         ) {
-            let entry = Entry(Date(), configuration)
-            completion(entry)
+            getEntry(for: configuration, in: context, completion: completion)
         }
 
         func getTimeline(
             for configuration: ConfigurationIntent,
             in context: Context,
-            completion: @escaping (Timeline<Entry>) -> ()
+            completion: @escaping (Timeline<Entry>) -> Void
         ) {
-            var entries: [Entry] = []
-            let currentDate = Date()
+            if UserDefaults.General.backgroundRefresh {
+                let started = Service.shared.refresh { status, count in
+                    if status == .requestMatch {
+                        UNUserNotificationCenter.push(
+                            NSLocalizedString("notification.refresh.requiresMatch", comment: "Manually Match Required"),
+                            NSLocalizedString("notification.refresh.requiresMatch.desc", comment: "Manually Match Required Description")
+                        )
+                    } else if count > 0 {
+                        UNUserNotificationCenter.push(
+                            NSLocalizedString("notification.refresh.refreshFinished", comment: "Refresh Finished"),
+                            String(format: NSLocalizedString("notification.refresh.refreshFinished.desc", comment: "Refresh Finished Description"), count)
+                        )
+                    }
+                    getEntry(for: configuration, in: context) { entry in
+                        completion(.init(entries: [ entry ], policy: .atEnd))
+                    }
+                }
+                if !started {
+                    getEntry(for: configuration, in: context) { entry in
+                        completion(.init(entries: [ entry ], policy: .atEnd))
+                    }
+                }
+            } else {
+                getEntry(for: configuration, in: context) { entry in
+                    completion(.init(entries: [ entry ], policy: .atEnd))
+                }
+            }
+        }
+        
+        private func getEntry(
+            for configuration: ConfigurationIntent,
+            in context: Context,
+            completion: @escaping (Entry) -> Void
+        ) {
             let predicate: NSPredicate?
             switch configuration.status {
-                case .unknown: predicate = nil
-                case .all: predicate = nil
                 case .pending: predicate = Umi.shared.status[.pending]!.predicate
                 case .accepted: predicate = Umi.shared.status[.accepted]!.predicate
                 case .rejected: predicate = Umi.shared.status[.rejected]!.predicate
+                default: predicate = nil
             }
+            let now = Date()
             let nominations = Dia.shared.nominations(matches: predicate)
-            if nominations.isEmpty {
-                entries.append(.init(currentDate, configuration, empty: true))
-            } else {
-                for index in 0 ..< 10 {
-                    guard let entryDate = Calendar.current.date(
-                        byAdding: .minute, value: index * 30, to: currentDate
-                    ) else {
-                        continue
-                    }
-                    entries.append(.init(entryDate, configuration, nominations[Int.random(in: 0 ..< nominations.count)]))
-                }
+            guard !nominations.isEmpty else {
+                completion(.init(now, empty: true))
+                return
             }
-            let timeline = Timeline(entries: entries, policy: .atEnd)
-            completion(timeline)
+            let nomination = nominations[Int.random(in: 0 ..< nominations.count)]
+            URLSession.shared.dataTask(with: nomination.imageURL) { data in
+                completion(.init(now, image: data ?? .init(), nomination: nomination))
+            }
         }
     }
     
     struct Entry: TimelineEntry {
         let date: Date
-        let configuration: NominationWidgetConfigurationIntent
         
         let empty: Bool
         
@@ -74,19 +99,14 @@ struct NominationWidget: Widget {
         
         init(
             _ date: Date,
-            _ configuration: ConfigurationIntent,
-            _ nomination: Nomination
+            image: Data,
+            nomination: Nomination
         ) {
             self.date = date
-            self.configuration = configuration
             empty = false
             id = nomination.id
             title = nomination.title
-            if let url = URL(string: nomination.imageURL) {
-                imageData = (try? Data(contentsOf: url)) ?? Data()
-            } else {
-                imageData = Data()
-            }
+            imageData = image
             let status = nomination.statusData
             statusIcon = status.icon
             statusColor = status.color
@@ -94,12 +114,9 @@ struct NominationWidget: Widget {
         
         init(
             _ date: Date,
-            _ configuration: ConfigurationIntent,
             empty: Bool = false
         ) {
             self.date = date
-            self.configuration = configuration
-            
             self.empty = empty
             
             id = ""
@@ -170,8 +187,13 @@ struct NominationWidget: Widget {
 #if DEBUG
 struct NominationWidget_Previews: PreviewProvider {
     static var previews: some View {
+        let nomination = Dia.preview.nominations[0]
         NominationWidget.EntryView(
-            entry: .init(Date(), .init(), Dia.preview.nominations[0])
+            entry: .init(
+                Date(),
+                image: (try? Data(contentsOf: URL(string: nomination.imageURL)!)) ?? Data(),
+                nomination: nomination
+            )
         )
         .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
