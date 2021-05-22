d75bc641016104889ec722bfca9f2ef716e6b896
lucka-me <i@lucka.moe>
2021-05-22 12:50:43 +0800

    Simplify UNUserNotificationCenter.push(:,:)

--- Shared/Extensions/UNUserNotificationCenter.swift
+++ Shared/Extensions/UNUserNotificationCenter.swift
@@ -8,22 +8,15 @@
 import UserNotifications
 
 extension UNUserNotificationCenter {
-    static func requestAuthorization() {
-        Self.current().requestAuthorization(options: [ .alert, .provisional ]) { _, _ in
-            // It's fine to work without notification
-        }
-    }
-    
     static func push(_ title: String, _ body: String) {
-        let center = Self.current()
-        center.getNotificationSettings { settings in
-            if settings.authorizationStatus == .authorized || settings.authorizationStatus == .provisional {
-                let content = UNMutableNotificationContent()
-                content.title = title
-                content.body = body
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-                center.add(request, withCompletionHandler: nil)
+        current().requestAuthorization(options: [ .alert, .provisional ]) { granted, _ in
+            guard granted else {
+                return
             }
+            let content = UNMutableNotificationContent()
+            content.title = title
+            content.body = body
+            current().add(.init(identifier: UUID().uuidString, content: content, trigger: nil))
         }
     }
 }
