1ceedc7a8e711d8972a106d871e9c4fc855caaa7
lucka-me <i@lucka.moe>
2021-05-22 12:40:12 +0800

    Fix empty match sheet

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
     var body: some View {
         navigationView
             .sheet(isPresented: $navigation.showMatchView) {
-                EmptyView()
+                MatchView()
             }
     }
     
