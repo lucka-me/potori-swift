56403c986c53c687bbce9ebd9a6a8ac2039dd373
lucka-me <i@lucka.moe>
2021-11-06 11:03:22 +0800

    Fix FixedSizeIconLabelStyle

--- Shared/Views/Basic/ViewStyles.swift
+++ Shared/Views/Basic/ViewStyles.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct FixedSizeIconLabelStyle: LabelStyle {
+    
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
+    
     func makeBody(configuration: Configuration) -> some View {
         HStack {
             configuration.icon
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                .aspectRatio(1, contentMode: .fit)
+                .frame(width: iconSize, height: iconSize, alignment: .center)
             configuration.title
                 .lineLimit(1)
+        }
+    }
+    
+    private var iconSize: CGFloat {
+        switch dynamicTypeSize {
+            case .xSmall: return 16
+            case .small: return 21
+            case .medium: return 24
+            case .large: return 28
+            case .xLarge: return 32
+            case .xxLarge: return 36
+            case .xxxLarge: return 40
+            case .accessibility1: return 48
+            case .accessibility2: return 52
+            case .accessibility3: return 60
+            case .accessibility4: return 66
+            case .accessibility5: return 72
+            default: return 24
         }
     }
 }
